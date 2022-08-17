3c8007120e8a7e21d5bd822f0023acc11db5feec
Uyen <uyen.nguyenminhduy@gmail.com>
2022-08-17 08:38:17 +0700

    Initial Commit

---
+++ RMIT Casino/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RMIT Casino
+//
+//  Created by Uyen Nguyen Minh Duy on 17/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RMIT Casino/RMIT_CasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMIT_CasinoApp.swift
+//  RMIT Casino
+//
+//  Created by Uyen Nguyen Minh Duy on 17/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RMIT_CasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
